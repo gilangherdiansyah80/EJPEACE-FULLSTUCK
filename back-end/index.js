7cfe8ca6e523f5cb63ffcf25f39f606242b3698e
gilangherdiansyah80 <gilangherdiansyah404@gmail.com>
2025-07-01 22:14:08 +0700

    test

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import db from "./conection.js";
 import response from "./response.js";
-import midtransClient from "midtrans-client";
+// import midtransClient from "midtrans-client";
 import { Invoice } from "xendit-node";
 
 const app = express();
@@ -389,159 +389,159 @@ app.delete("/api/carts/:id", (req, res) => {
 });
 
 // Midtrans Snap API setup
-let snap = new midtransClient.Snap({
-  isProduction: false, // Set to true if you want Production Environment (accept real transaction).
-  serverKey: "SB-Mid-server-wHln2cytDNe0mhRtqIcQ_Gk3",
-});
+// let snap = new midtransClient.Snap({
+//   isProduction: false, // Set to true if you want Production Environment (accept real transaction).
+//   serverKey: "SB-Mid-server-wHln2cytDNe0mhRtqIcQ_Gk3",
+// });
 
-// Endpoint untuk membuat transaksi Midtrans
-app.post("/api/create-transaction/booking", (req, res) => {
-  let { booking_id, total, name, email, phone, duration } = req.body;
+// // Endpoint untuk membuat transaksi Midtrans
+// app.post("/api/create-transaction/booking", (req, res) => {
+//   let { booking_id, total, name, email, phone, duration } = req.body;
 
-  let parameter = {
-    transaction_details: {
-      order_id: booking_id,
-      gross_amount: total,
-    },
-    credit_card: {
-      secure: true,
-    },
-    item_details: [
-      {
-        id: booking_id,
-        price: 50000,
-        quantity: duration,
-        name: "jam x Rp. 50.000",
-      },
-    ],
-    customer_details: {
-      name: name,
-      email: email,
-      phone: phone,
-    },
-  };
+//   let parameter = {
+//     transaction_details: {
+//       order_id: booking_id,
+//       gross_amount: total,
+//     },
+//     credit_card: {
+//       secure: true,
+//     },
+//     item_details: [
+//       {
+//         id: booking_id,
+//         price: 50000,
+//         quantity: duration,
+//         name: "jam x Rp. 50.000",
+//       },
+//     ],
+//     customer_details: {
+//       name: name,
+//       email: email,
+//       phone: phone,
+//     },
+//   };
 
-  snap
-    .createTransaction(parameter)
-    .then((transaction) => {
-      // Transaction token
-      let transactionToken = transaction.token;
-      res.json({ transactionToken: transactionToken });
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error.message });
-    });
-});
+//   snap
+//     .createTransaction(parameter)
+//     .then((transaction) => {
+//       // Transaction token
+//       let transactionToken = transaction.token;
+//       res.json({ transactionToken: transactionToken });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: error.message });
+//     });
+// });
 
-app.post("/api/create-transaction/package", (req, res) => {
-  let { order_id, total, name, email, noTelepon, level } = req.body;
-  console.log(total);
+// app.post("/api/create-transaction/package", (req, res) => {
+//   let { order_id, total, name, email, noTelepon, level } = req.body;
+//   console.log(total);
 
-  let parameter = {
-    transaction_details: {
-      order_id: order_id,
-      gross_amount: total,
-    },
-    credit_card: {
-      secure: true,
-    },
-    customer_details: {
-      name: name,
-      email: email,
-      phone: noTelepon,
-    },
-  };
+//   let parameter = {
+//     transaction_details: {
+//       order_id: order_id,
+//       gross_amount: total,
+//     },
+//     credit_card: {
+//       secure: true,
+//     },
+//     customer_details: {
+//       name: name,
+//       email: email,
+//       phone: noTelepon,
+//     },
+//   };
 
-  snap
-    .createTransaction(parameter)
-    .then((transaction) => {
-      // Transaction token
-      let token = transaction.token;
-      res.json({ token: token });
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error.message });
-    });
-});
+//   snap
+//     .createTransaction(parameter)
+//     .then((transaction) => {
+//       // Transaction token
+//       let token = transaction.token;
+//       res.json({ token: token });
+//     })
+//     .catch((error) => {
+//       res.status(500).json({ error: error.message });
+//     });
+// });
 
-// Endpoint untuk menerima notifikasi pembayaran dari Midtrans
-app.post("/api/payment-notification", (req, res) => {
-  const data = req.body;
-  console.log("Received payment data:", data); // Log data yang diterima
+// // Endpoint untuk menerima notifikasi pembayaran dari Midtrans
+// app.post("/api/payment-notification", (req, res) => {
+//   const data = req.body;
+//   console.log("Received payment data:", data); // Log data yang diterima
 
-  const {
-    id = Math.floor(Math.random() * 1000),
-    order_id,
-    name,
-    email,
-    phone,
-    date,
-    timeStart,
-    timeEnd,
-    duration,
-    total,
-    status,
-  } = data;
+//   const {
+//     id = Math.floor(Math.random() * 1000),
+//     order_id,
+//     name,
+//     email,
+//     phone,
+//     date,
+//     timeStart,
+//     timeEnd,
+//     duration,
+//     total,
+//     status,
+//   } = data;
 
-  let sql = `INSERT INTO bookings (id, order_id, name, email, phone, date, time_start, time_end, duration, price, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE name=?, email=?, phone=?, date=?, time_start=?, time_end=?, duration=?, price=?, status=?`;
+//   let sql = `INSERT INTO bookings (id, order_id, name, email, phone, date, time_start, time_end, duration, price, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE name=?, email=?, phone=?, date=?, time_start=?, time_end=?, duration=?, price=?, status=?`;
 
-  let params = [
-    id,
-    order_id,
-    name,
-    email,
-    phone,
-    date,
-    timeStart,
-    timeEnd,
-    duration,
-    total,
-    status,
-    name,
-    email,
-    phone,
-    date,
-    timeStart,
-    timeEnd,
-    duration,
-    total,
-    status,
-  ];
+//   let params = [
+//     id,
+//     order_id,
+//     name,
+//     email,
+//     phone,
+//     date,
+//     timeStart,
+//     timeEnd,
+//     duration,
+//     total,
+//     status,
+//     name,
+//     email,
+//     phone,
+//     date,
+//     timeStart,
+//     timeEnd,
+//     duration,
+//     total,
+//     status,
+//   ];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      console.error("Failed to insert or update booking record", err);
-      res
-        .status(500)
-        .json({ error: "Failed to insert or update booking record" });
-    } else {
-      console.log("Booking record inserted or updated successfully");
-      res.status(200).send("OK");
-    }
-  });
-});
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       console.error("Failed to insert or update booking record", err);
+//       res
+//         .status(500)
+//         .json({ error: "Failed to insert or update booking record" });
+//     } else {
+//       console.log("Booking record inserted or updated successfully");
+//       res.status(200).send("OK");
+//     }
+//   });
+// });
 
-// Endpoint POST untuk membuat payments baru
-app.post("/api/payment-package", (req, res) => {
-  const data = req.body;
-  console.log("Received payment data:", data); // Log data yang diterima
+// // Endpoint POST untuk membuat payments baru
+// app.post("/api/payment-package", (req, res) => {
+//   const data = req.body;
+//   console.log("Received payment data:", data); // Log data yang diterima
 
-  const { user_id, id_paket, tanggal_payment, tanggal_selesai } = data;
+//   const { user_id, id_paket, tanggal_payment, tanggal_selesai } = data;
 
-  let sql = `INSERT INTO siswa (user_id, id_paket, tanggal_payment, tanggal_selesai) VALUES (?, ?, ?, ?)`;
+//   let sql = `INSERT INTO siswa (user_id, id_paket, tanggal_payment, tanggal_selesai) VALUES (?, ?, ?, ?)`;
 
-  let params = [user_id, id_paket, tanggal_payment, tanggal_selesai];
+//   let params = [user_id, id_paket, tanggal_payment, tanggal_selesai];
 
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      console.error("Failed to insert payment record", err);
-      res.status(500).json({ error: "Failed to insert payment record" });
-    } else {
-      console.log("Payment record inserted successfully");
-      res.status(200).send("OK");
-    }
-  });
-});
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       console.error("Failed to insert payment record", err);
+//       res.status(500).json({ error: "Failed to insert payment record" });
+//     } else {
+//       console.log("Payment record inserted successfully");
+//       res.status(200).send("OK");
+//     }
+//   });
+// });
 
 // Endpoint POST untuk membuat user baru
 app.post("/api/create-users", (req, res) => {
